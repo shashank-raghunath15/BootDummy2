34112652f99af00fe6bd04f62dca199ea4fc4352
Shashank Raghunath <shashank.raghunath15@gmail.com>
2017-01-04 17:06:40 +0530

    ddd

--- src/main/java/org/dummy/controller/IndexController.java
+++ src/main/java/org/dummy/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping("/BootDummy/")
+	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
